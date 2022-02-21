8cffef159537055f2b1e2b8a3792ed1a3c08235a
pankaj kore <korep95@gmail.com>
2022-02-21 23:05:52 +0530

    [pankaj]added size method and implemented tree

--- src/com/BridgeLabz/BinarySearchTree/MyBinarySTreeMain.java
+++ src/com/BridgeLabz/BinarySearchTree/MyBinarySTreeMain.java
@@ -7,7 +7,18 @@ public class MyBinarySTreeMain {
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
-        myBinaryTree.printBinaryTree();
+        myBinaryTree.add(22);
+        myBinaryTree.add(60);
+        myBinaryTree.add(40);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
 
+        myBinaryTree.printBinaryTree();
+        System.out.println("The Size of the Binary Tree is : "+myBinaryTree.getSize());
     }
 }
