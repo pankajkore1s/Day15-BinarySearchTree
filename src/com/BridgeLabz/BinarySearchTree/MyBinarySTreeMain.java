ffe5eb8c936b94154ee0e33921105d7e0edcbc8e
pankaj kore <korep95@gmail.com>
2022-02-21 22:59:26 +0530

    [pankaj]created BST by adding 56,30,70

---
+++ src/com/BridgeLabz/BinarySearchTree/MyBinarySTreeMain.java
@@ -0,0 +1,13 @@
+package com.BridgeLabz.BinarySearchTree;
+
+public class MyBinarySTreeMain {
+    public static void main(String[] args) {
+        System.out.println("**Welcome to Data Structure : Binary Search Tree**");
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.printBinaryTree();
+
+    }
+}
